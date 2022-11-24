ae8c0460c380af6eacd10a7885a9d86814415b9d
nicky132 <hhq87776153@hotmail.com>
2022-11-24 23:06:25 +0800

    修改文件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,11 +22,7 @@
     </ul>
     <h3>Ecosystem</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <div>2314434</div>
     </ul>
   </div>
 </template>
